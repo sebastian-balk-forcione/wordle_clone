e9c7e3421871f7bc9780b070a761240bcec31274
Sebastian Balk-Forcione <sbalkforcione@gmail.com>
2023-05-09 13:04:58 -0400

    Grid

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-
+import styled from "styled-components";
 import Keyboard from "./Keyboard";
+import Board from "./Board";
 
 const App = () => {
   const [word, setWord] = useState([]);
@@ -17,10 +18,21 @@ const App = () => {
   }, []);
 
   return (
-    <>
-      <Keyboard></Keyboard>
-    </>
+    <Wrapper>
+      <div>
+        <Board />
+        <Keyboard></Keyboard>
+      </div>
+    </Wrapper>
   );
 };
 
 export default App;
+
+const Wrapper = styled.div`
+  max-width: 100vw;
+  max-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
---
+++ client/src/Board.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+const Board = () => {
+  const squares = [];
+  for (let i = 0; i < 30; i++) {
+    squares.push(<Square />);
+  }
+
+  return (
+    <Wrapper>
+      {squares.map((i) => {
+        return i;
+      })}
+    </Wrapper>
+  );
+};
+
+export default Board;
+
+const Wrapper = styled.div`
+  max-width: 20vw;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: repeat(6, 1fr);
+  grid-column-gap: 10px;
+  grid-row-gap: 10px;
+`;
+
+const Square = styled.div`
+  width: 30px;
+  height: 30px;
+  border: 2px solid pink;
+  display: inline;
+`;
